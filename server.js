a53470e14192934fc1e11c7b0c09eb68f045549d
Suhail <suhailcollegemail@gmail.com>
2022-11-20 18:50:14 +0530

    rwwrfwr

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
     // console.log(req.files)
     let d = await mergePdfs(path.join(__dirname , req.files[0].path),path.join(__dirname , req.files[1].path));
-    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    res.redirect(`https://pdf-69-merger.herokuapp.com/static/${d}.pdf`)
     // res.send({data: req.files})
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
