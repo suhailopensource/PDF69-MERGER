412791a91f91097f5c53a60a30a7fbbf24eb49c8
Suhail <suhailcollegemail@gmail.com>
2022-11-26 14:28:35 +0530

    yjkuyjkujktuj

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path') 
 const app = express()
-const PORT = process.env.PORT || 3000;
 const multer  = require('multer')
+const PORT = process.env.PORT || 3000;
 const {mergePdfs}  = require('./merge')
 const upload = multer({ dest: 'uploads/' })
 app.use('/static', express.static('public'))
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
     // console.log(req.files)
     let d = await mergePdfs(path.join(__dirname , req.files[0].path),path.join(__dirname , req.files[1].path));
-    res.redirect(`https://pdf-69-merger.herokuapp.com/static/${d}.pdf`)
+    res.redirect(`http://localhost:3000/static/${d}.pdf`)
     // res.send({data: req.files})
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
